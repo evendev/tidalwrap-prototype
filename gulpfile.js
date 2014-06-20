8810e9834749076f11713bd021e65e10d89ca4f5
Steve Jamesson <steve@groovywebdesign.com>
2014-06-20 12:06:33 -0400

    i18n changes

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,20 @@ var shell = require('gulp-shell');
 
 var secrets = require('./secrets.json');
 
+
 gulp.task('imagePerms', shell.task([
   'chmod -R 755 public/img/*'
 ]));
 
 gulp.task('compile', shell.task([
   'harp compile'
+]));
+
+gulp.task('serve-en', shell.task([
+  'NODE_ENV=en harp server'
+]));
+gulp.task('serve-es', shell.task([
+  'NODE_ENV=es harp server'
 ]));
 
 gulp.task('rsync', function() {
